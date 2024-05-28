48c1cb444761cd5bce38ac85580a7a003534e8cf
Theodor Steiner <theodor.steiner@linecorp.com>
2024-05-28 10:31:18 +0900

    feat: complete map exercises

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 )
 
+const (
+	word       = "whale"
+	definition = "big mammal living in the ocean"
+)
+
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
@@ -19,12 +24,30 @@ func assertStrings(t testing.TB, got, want string) {
 	}
 }
 
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	retrieved, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, retrieved, definition)
+}
+
+func assertWordNotInDictionary(t *testing.T, dictionary Dictionary, word string) {
+	t.Helper()
+	_, err := dictionary.Search(word)
+	if err == nil {
+		t.Fatal("should not find non-existing word:", err)
+	}
+	assertError(t, err, ErrNotFound)
+}
+
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	dictionary := Dictionary{word: definition}
 
 	t.Run("known word", func(t *testing.T) {
-		got, err := dictionary.Search("test")
-		want := "this is just a test"
+		got, err := dictionary.Search(word)
+		want := definition
 
 		assertError(t, err, nil)
 
@@ -32,11 +55,50 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		val, err := dictionary.Search("unknown")
 
 		assertError(t, err, ErrNotFound)
 
-		assertStrings(t, err.Error(), want)
+		assertStrings(t, val, "")
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("add new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("try add existing word", func(t *testing.T) {
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "overriding word")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("update existing", func(t *testing.T) {
+		dictionary := Dictionary{word: "old definition"}
+		err := dictionary.Update(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("update non-existing", func(t *testing.T) {
+		dictionary := Dictionary{}
+		err := dictionary.Update(word, definition)
+		assertError(t, err, ErrWordDoesNotExist)
+		assertWordNotInDictionary(t, dictionary, word)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("update existing", func(t *testing.T) {
+		dictionary := Dictionary{word: definition}
+		dictionary.Delete(word)
+		assertWordNotInDictionary(t, dictionary, word)
 	})
 }
--- maps/notes.md
+++ maps/notes.md
@@ -11,4 +11,40 @@
 - lookup: `value, ok := dictionary[key]`
   - always returns string (`""` if not found)
   - second return is `boolean` that indicates if map lookup successful
+- key/value pairs can be deleted using `delete(map,key)`
+- maps can be globally modified even if passed by reference
+  - A map value is a pointer to a runtime.hmap structure
+  - therefore maps can be nil
 
+  -> never not-initialize a map ~var m map[string]string]~
+  instead initialize with an empty collection:
+  `var m = map[string]string{}`
+
+### best practice
+
+create error constants for sentinel errors:
+
+> Sentinel errors are user defined errors that indicated very specific events that you, as a developer, anticipate & identify as adequately important to define and specify. As such, you declare them at the package level and, in doing so, imply that your package functions may return these errors (thereby committing you in the future to maintain these errors as others depending on your package will be checking for them).
+
+see: [digital ocean sentinel errors](https://www.digitalocean.com/community/tutorials/how-to-add-extra-information-to-errors-in-go#handling-specific-errors-using-sentinel-errors) 
+
+A sentinel error can be defined as a var, 
+```go
+var MySentinelError = error.New("MySentinelError")
+```
+however that renders it mutable.
+There is however a way to declare sentinel errors in an immutable manner:
+```go
+type ErrorType string // declare your ErrorType based on the default string type
+
+// add an `Error` method to your ErrorType, to implement the error interface
+func (e ErrorType) Error() string {
+  string(e)
+}
+
+// you can now declare sentinel errors in an immutable manner
+const (
+  MySentinelError = "Oh no!"
+  MyOtherBadError = "Oh hell no!"
+)
+```
