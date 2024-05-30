1767636eed046a3691de48c905e96ed31c9e6d2d
Theodor Steiner <theodor.steiner@linecorp.com>
2024-05-30 21:56:37 +0900

    feat: finish di exercise

--- di/notes.md
+++ di/notes.md
@@ -1,3 +1,6 @@
 ## What I learned/noticed:
 
 ### Dependency Injection
+
+by accepting a generic interface, such as io.Writer to our function, 
+we allow for various implementations (or mocks) to be injected into it
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	di "learngowithtests/di"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	di.Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(di.MyGreeterHandler)))
 }
